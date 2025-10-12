a96705eaf7d5646ac46a3bf01f00cffa32dd20f4
abdelazizhany66 <adhmh080@gmail.com>
2025-10-12 23:15:26 +0200

    second issue count positive and sum negative number in array

--- count-positive-sum-negative.js
+++ count-positive-sum-negative.js
@@ -1,14 +1,17 @@
 function countPositiveSumNegative(arr) {
-  let countPositive = [];
-  let sumNegative = 0;
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      countPositive.push(arr[i]);
-    } else {
-      sumNegative += arr[i];
-    }
-  }
-  return [countPositive.length, sumNegative];
+  // let countPositive = [];
+  // let sumNegative = 0;
+  // for (let i = 0; i < arr.length; i++) {
+  //   if (arr[i] > 0) {
+  //     countPositive.push(arr[i]);
+  //   } else {
+  //     sumNegative += arr[i];
+  //   }
+  // }
+  // return [countPositive.length, sumNegative];
+  const countPositive =arr.filter((e)=> e>0).length 
+  const sumNegative = arr.filter((e)=> e<0).reduce((acc,current)=> acc+current,0)
+  return [countPositive,sumNegative]
 }
 
 console.log(countPositiveSumNegative([-1, -2, -3, 1, 2, 3]));
