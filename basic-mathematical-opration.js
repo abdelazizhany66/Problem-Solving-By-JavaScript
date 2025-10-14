b143620ce1b61deccbcf27c883cf877456da06bb
abdelazizhany66 <adhmh080@gmail.com>
2025-10-14 22:04:14 +0200

    new issue solve 2

--- basic-mathematical-opration.js
+++ basic-mathematical-opration.js
@@ -1,8 +1,10 @@
 function basicOpration(opration, value1, value2) {
-  if (opration == '/') return value1 / value2;
-  if (opration == '+') return value1 + value2;
-  if (opration == '-') return value1 - value2;
-  if (opration == '*') return value1 * value2;
+  // if (opration == '/') return value1 / value2;
+  // if (opration == '+') return value1 + value2;
+  // if (opration == '-') return value1 - value2;
+  // if (opration == '*') return value1 * value2;
+
+  return eval(value1 + opration + value2)
 }
 
 console.log(basicOpration('+', 1, 2));
