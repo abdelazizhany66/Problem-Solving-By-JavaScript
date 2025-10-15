b5a9881a165d46614243d89c0fcb82665c096512
abdelazizhany66 <adhmh080@gmail.com>
2025-10-15 23:16:04 +0200

    add new solve

--- to-square-root-or-not-to-square-root.js
+++ to-square-root-or-not-to-square-root.js
@@ -1,8 +1,18 @@
 function squareOrSquareRoot(arr) {
-  return arr.map((number) =>
-    // .isInteger return true or false if integre return true else return false
-    Number.isInteger(Math.sqrt(number)) ? Math.sqrt(number) : number * number
-  );
+  // return arr.map((number) =>
+  //   // .isInteger return true or false if integre return true else return false
+  //   Number.isInteger(Math.sqrt(number)) ? Math.sqrt(number) : number * number
+  // );
+
+  resultArr = [];
+  for (let i = 0; i < arr.length; i++) {
+    if (Number.isInteger(Math.sqrt(arr[i]))) {
+      resultArr.push(Math.sqrt(arr[i]));
+    } else {
+      resultArr.push(arr[i] * arr[i]);
+    }
+  }
+  return resultArr;
 }
 
 console.log(squareOrSquareRoot([1, 2, 3, 7, 4]));
