c28cce16da60242c8acd55d1ec29594bdcecde98
abdelazizhany66 <adhmh080@gmail.com>
2025-10-23 06:54:44 +0200

    second issue get avarge numbers in the array

--- calcu-avg-in-arr.js
+++ calcu-avg-in-arr.js
@@ -1,8 +1,12 @@
 function avarge(arr) {
-  let totalNum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    totalNum += arr[i];
-  }
-  return totalNum / arr.length;
+  if (arr === null || arr.length === 0) return 0;
+  const ele = arr.reduce((acc, current) => acc + current, 0);
+  return ele / arr.length;
+
+  // let totalNum = 0;
+  // for (let i = 0; i < arr.length; i++) {
+  //   totalNum += arr[i];
+  // }
+  // return totalNum / arr.length;
 }
-console.log(avarge([1,2,3]));
+console.log(avarge([]));
