ffc3ec5044dfb5db184a762ecbd42115a87b7ec9
abdelazizhany66 <adhmh080@gmail.com>
2025-10-25 08:17:33 +0200

    issue when client enter her language the program return your language wlecom

---
+++ welcom.js
@@ -0,0 +1,29 @@
+function greet(language) {
+  const languages = {
+    english: 'Welcome',
+    czech: 'Vitejte',
+    danish: 'Velkomst',
+    dutch: 'Welkom',
+    estonian: 'Tere tulemast',
+    finnish: 'Tervetuloa',
+    flemish: 'Welgekomen',
+    french: 'Bienvenue',
+    german: 'Willkommen',
+    irish: 'Failte',
+    italian: 'Benvenuto',
+    latvian: 'Gaidits',
+    lithuanian: 'Laukiamas',
+    polish: 'Witamy',
+    spanish: 'Bienvenido',
+    swedish: 'Valkommen',
+    welsh: 'Croeso'
+  };
+  if (languages.hasOwnProperty(language)) {
+    return languages[language];
+  }else{
+    return languages['english']
+  }
+}
+
+
+console.log(greet('Arabic'))
