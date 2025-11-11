2fa32a03eb6744aaa7d057c01d984354d99358de
Abdel-Aziz Hany <abdelazizhany06@gmail.com>
2025-11-11 15:18:13 +0200

    add comment

--- opposites-attract.js
+++ opposites-attract.js
@@ -1,10 +1,12 @@
 function loveFun(flower1, flower2) {
+  //solution 1
   // if (flower1 % 2 !== flower2 % 2) {
   //   return true;
   // } else {
   //   return false;
   // }
   // return flower1 % 2 !== flower2 % 2;
-  return (flower1 + flower2) % 2 != 0
+  // solution 2
+  return (flower1 + flower2) % 2 != 0;
 }
 console.log(loveFun(3, 3));
